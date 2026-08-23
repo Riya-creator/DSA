faab3eb2943a169b9d0085cf2352018b3b37fccc
Riya <riya.connect56@gmail.com>
2026-08-23 15:15:52 +0530

    Update Find_Primes.cpp

--- Array/Prime_Factors/Find_Primes.cpp
+++ Array/Prime_Factors/Find_Primes.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 
 void printPrimeFactors(int n) {
     // 1. Handle all the factors of 2
     while (n % 2 == 0) {
-        std::cout << 2 << " ";
+        cout << 2 << " ";
         n = n / 2;
     }
 
@@ -11,25 +12,25 @@ void printPrimeFactors(int n) {
     // We only need to check up to the square root of n
     for (int i = 3; i * i <= n; i += 2) {
         while (n % i == 0) {
-            std::cout << i << " ";
+            cout << i << " ";
             n = n / i;
         }
     }
 
     // 3. If n is still greater than 2, then n itself is prime
     if (n > 2) {
-        std::cout << n << " ";
+        cout << n << " ";
     }
 }
 
 int main() {
     int number;
-    std::cout << "Enter a number: ";
-    std::cin >> number;
+    cout << "Enter a number: ";
+    cin >> number;
 
-    std::cout << "Prime factors: ";
+    cout << "Prime factors: ";
     printPrimeFactors(number);
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 }
