33fc42bfa7d0fdcc91bdc42f38474c675c5361b3
Riya <riya.connect56@gmail.com>
2026-08-23 11:17:28 +0530

    Create Smaller_Way.cpp

---
+++ Array/Prime_Factors/Smaller_Way.cpp
@@ -0,0 +1,23 @@
+void PrimeFactors(unordered_map<int,int>& prime, int nums, bool remove) {
+        for(int i = 2; i * i <= nums; i++) {
+            if(nums % i == 0) {
+                if(!remove) prime[i]++;
+                else {
+                    prime[i]--;
+                    if(prime[i] == 0) prime.erase(i);
+                }
+
+                while(nums % i == 0)
+                    nums /= i;
+            }
+        }
+
+        if(nums > 1) {
+            if(!remove) prime[nums]++;
+            else {
+                prime[nums]--;
+                if(prime[nums] == 0) prime.erase(nums);
+            }
+        }
+    }
+//time:O(root nums)
